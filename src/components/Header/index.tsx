a532f711acebf9e2472b51149f05bfb3afdc1da6
Your Name <you@example.com>
2022-06-01 13:34:11 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+
+import Image from 'next/image';
+import logoImg from '../../../public/images/logo.svg'
+import { SignInButton } from '../SignInButton';
+import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
+
+export function Header(){
+
+  
+
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src={logoImg} alt="ig.news"/>
+        <nav>
+          <ActiveLink activeClassName={styles.active} href='/'>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href='/posts'>
+            <a>Posts</a>
+          </ActiveLink>  
+        </nav>
+        <SignInButton />
+      </div>
+      
+    </header>
+  )
+}
